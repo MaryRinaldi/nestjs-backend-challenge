acceb5d051f9e2808d9e4b971ab4a6136a019604
MaryRinaldi <dr.rinaldimaria@gmail.com>
2025-07-14 17:44:57 +0200

    Implement user registration for Mongoose

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './schemas/user.schema';
 
-@Controller('users')
-export class UsersController {}
+@Controller('users') // prefisso di route a /users
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('/register') // richieste POST a /users/register
+  @HttpCode(HttpStatus.CREATED)
+  async register(@Body() createUserDto: CreateUserDto): Promise<Omit<User, 'password'>> {
+    const user = await this.usersService.create(createUserDto);
+
+    const { password, ...result } = user.toObject();
+    
+    return result;
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User, UserDocument } from './schemas/user.schema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async create(createUserDto: CreateUserDto): Promise<UserDocument> {
+    const { email, password, ...rest } = createUserDto;
+    // Controllo se un utente con questa email esiste
+    const existingUser = await this.userModel.findOne({ email }).exec();
+    if (existingUser) {
+      throw new ConflictException('Un utente con questa email esiste già.');
+    }
+    //  Hash della password
+    const salt = await bcrypt.genSalt(16); 
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const createdUser = new this.userModel({
+      email,
+      password: hashedPassword,
+      ...rest,
+    });
+
+    try {
+      return await createdUser.save();
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Si è verificato un errore durante la creazione dell\'utente.',
+      );
+    }
+  }
+}
