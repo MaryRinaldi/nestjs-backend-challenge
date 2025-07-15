b19ec1b34582ecc4f9ec4dafe7a5669e85c9169c
MaryRinaldi <dr.rinaldimaria@gmail.com>
2025-07-15 20:06:59 +0200

    Error in reading User as Document fix

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,10 +1,8 @@
 import { UserDocument } from '../../users/schemas/user.schema';
 
-// Dichiariamo a TypeScript che vogliamo aggiungere una proprietà al tipo Request di Express
 declare global {
   namespace Express {
     export interface Request {
-      // La proprietà user è opzionale (?) e conterrà il nostro UserDocument
       user?: UserDocument;
     }
   }
