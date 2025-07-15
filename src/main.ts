998c98222ae87c5815261f3eb29481b9b85e5a81
MaryRinaldi <dr.rinaldimaria@gmail.com>
2025-07-15 21:07:43 +0200

    swagger config

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, Logger } from '@nestjs/common'; //valida e logs
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'; //per vedere endpoints e data 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -10,6 +11,19 @@ async function bootstrap() {
   const logger = new Logger('Bootstrap');
 
   app.useGlobalPipes(new ValidationPipe());
+
+  // swagger config
+  const config = new DocumentBuilder()
+    .setTitle('LBD - Projects API')
+    .setDescription('API Documentation for users management and favorite projects.')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document); // UI disponibile a /api-docs
+  logger.log('📄 Swagger UI is available at /api-docs');
+
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT', { infer: true}) ?? 3000;
